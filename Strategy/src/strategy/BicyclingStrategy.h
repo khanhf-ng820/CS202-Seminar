2c66e22fd9d01bc3c35106ad48736628dda2d775
Khanh Nguyen <ngpkhanh08@gmail.com>
2026-08-29 18:20:50 +0700

    added Changes.md

--- Strategy/src/strategy/BicyclingStrategy.h
+++ Strategy/src/strategy/BicyclingStrategy.h
@@ -9,7 +9,7 @@ public:
         (void)origin;
         (void)destination;
         std::cout << " -> Mode: BICYCLING (Newly Added Strategy! - OCP Compliant)\n";
-        std::cout << " -> Algorithm: Dedicated bike lanes, elevation elevation profile optimization & flat routes.\n";
+        std::cout << " -> Algorithm: Dedicated bike lanes, elevation profile optimization & flat routes.\n";
         std::cout << " -> Estimated Time: 50 mins (17.1 km)\n";
     }
 };
